5844be1c4d0a9ffe9d5d17269a3e736e1ae479d0
alfaraday <arianaloreleifaraday@gmail.com>
2016-09-02 09:03:08 -0400

    fixed keydown, added text

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $('document').ready(function(){
+	$('.how-to').show('slow');
 	$('.ryu').mouseenter(function(){
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
@@ -6,5 +7,50 @@ $('document').ready(function(){
 	.mouseleave(function(){
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate({
+			'left': '900px'},
+			500,
+			function(){
+				$(this).stop();
+				$(this).hide();
+				$(this).css('left', '570px')
+			}
+		);
+	})
+	.mouseup(function(){
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
-});
+	$('body').keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-cool').show();
+		}
+	})
+	.keyup(function(e){
+		if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
+	});
+
+})
+function doIntro() {
+  $('.sf-logo').fadeIn(3500, function() {
+    $('.sf-logo').fadeOut(1000, function() {
+      $('.brought-by').fadeIn(1500, function() {
+        $('.brought-by').fadeOut(1000, function() {
+          $('.jquery-logo').fadeIn(1500, function() {
+            $('.jquery-logo').fadeOut(1500, function() {
+              $('.how-to').fadeIn(1000);
+            });
+          })
+        })
+      })
+    })
+  })
+};
